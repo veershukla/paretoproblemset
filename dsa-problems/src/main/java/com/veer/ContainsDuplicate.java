65babb9d093e9e2982cac7b13c5477aabe2a17b5
Veerendra Shukla <veshukla@gmail.com>
2025-04-23 23:27:23 -0700

    contains duplicate bruteforce

---
+++ dsa-problems/src/main/java/com/veer/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+package com.veer;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,2,5,4,6};
+        System.out.println(hasDuplicate(nums));
+    }
+
+    //bruteforce approach
+    public static boolean hasDuplicate(int[] nums) {
+        for( int i = 0; i<nums.length;i++) {
+            for( int j = i+1; j<nums.length;j++){
+                if( nums[i] ==nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
